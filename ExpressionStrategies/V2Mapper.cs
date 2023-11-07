6bbdd82ce7d58652258805b462b2adbe967207d3
lorow <smykupyka@gmail.com>
2023-11-08 00:04:29 +0100

    Implement PoC mapping for v2 params
    todo: think about simplifying mappers even further

--- ExpressionStrategies/V2Mapper.cs
+++ ExpressionStrategies/V2Mapper.cs
@@ -1,18 +1,29 @@
 ﻿using Microsoft.Extensions.Logging;
+using VRCFaceTracking;
 using VRCFaceTracking.Core.Params.Data;
+using VRCFaceTracking.Core.Types;
 
 namespace ETVRTrackingModule.ExpressionStrategies;
 
 public class V2Mapper : ImappingStategy
 {
+    private string[] singleEyeParamNames =
+    {
+        "EyeX",
+        "EyeY",
+        "EyeLid"
+    };
+    
     private Dictionary<string, float> parameterValues = new()
     {
         { "EyeX", 0f },
-        { "EyeLeftX", 0f },
-        { "EyeRightX", 0f },
-        { "EyeLeftY", 0f },
-        { "EyeRightY", 0f },
+        { "EyeY", 0f },
         { "EyeLid", 0f },
+        
+        { "EyeLeftX", 0f },
+        { "EyeLeftY", 0f },
+        { "EyeRightX", 0f },
+        { "EyeRightY", 0f },
         { "EyeLidLeft", 0f },
         { "EyeLidRight", 0f },
     };
@@ -29,10 +40,29 @@ public class V2Mapper : ImappingStategy
         if (parameterValues.ContainsKey(paramToMap))
         {
             parameterValues[paramToMap] = message.value;
+
+            var singleEyeMode = singleEyeParamNames.Contains(paramToMap);
+            UpdateVRCFTEyeData(ref UnifiedTracking.Data.Eye, ref UnifiedTracking.Data.Shapes, singleEyeMode);
         }
     }
 
-    public void UpdateVRCFTEyeData(ref UnifiedEyeData eyeData, ref UnifiedExpressionShape[] eyeShapes)
+    public void UpdateVRCFTEyeData(ref UnifiedEyeData eyeData, ref UnifiedExpressionShape[] eyeShapes, bool singleEyeMode = false)
     {
+        handleEyeGaze(ref eyeData, singleEyeMode);
+    }
+    
+    private void handleEyeGaze(ref UnifiedEyeData eyeData, bool singleEyeMode)
+    {
+        // todo, we can probably drop support but EyeX/EyeY but I'll leave it be for now
+        if (singleEyeMode)
+        {
+            var combinedGaze = new Vector2(parameterValues["EyeX"], parameterValues["EyeY"]);
+            eyeData.Left.Gaze = combinedGaze;
+            eyeData.Right.Gaze = combinedGaze;
+            return;
+        }
+
+        eyeData.Left.Gaze = new Vector2(parameterValues["EyeLeftX"], parameterValues["EyeLeftY"]);
+        eyeData.Right.Gaze = new Vector2(parameterValues["EyeRightX"], parameterValues["EyeRightY"]);
     }
 }
